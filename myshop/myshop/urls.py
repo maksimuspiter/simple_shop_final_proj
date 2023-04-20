61e79b3c29f68364ca64e05e621869bfe01d0840
Maks <maksimuspiter@gmail.com>
2023-04-20 18:28:43 +0300

    create products models first views + admin

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("shop", include("shop.urls", namespace="shop")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
