526698c48e2ed4a3a00e8a9ec7c1ad5995b14236
Maks <maksimuspiter@gmail.com>
2023-05-05 21:14:29 +0300

    add set coupon and chek activated_cupon

--- myshop/orders/urls.py
+++ myshop/orders/urls.py
@@ -6,4 +6,5 @@ app_name = "orders"
 
 urlpatterns = [
     path("create/", views.order_create, name="order_create"),
+    path("set_coupon/", views.set_coupon, name="set_coupon"),
 ]
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,8 +1,12 @@
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .models import Order, OrderItem
+from account.models import Account
+from shop.models import Cupon
+from django.views.decorators.http import require_POST
+import json
 
 
 def order_create(request):
@@ -34,4 +38,24 @@ def order_create(request):
             }
         )
 
-    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
+    activated_cupon = request.user.account.activated_cupon
+    return render(
+        request,
+        "orders/order/create.html",
+        {"cart": cart, "form": form, 'ass': 'hello', "activated_cupon": activated_cupon},
+    )
+
+
+@require_POST
+def set_coupon(request):
+    coupon_code = request.POST.get("coupon_code")
+    coupon = Cupon.objects.filter(code=coupon_code).first()
+    result = False
+    if coupon:
+        request.user.account.activated_cupon = coupon
+        request.user.account.save()
+        result = True
+    return HttpResponse(
+        json.dumps({"result": result, "coupon_code": coupon_code}),
+        content_type="application/json",
+    )
