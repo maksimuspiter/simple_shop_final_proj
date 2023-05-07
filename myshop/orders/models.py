7004b892cd028e1408da942414da8f62e2a0f465
Maks <maksimuspiter@gmail.com>
2023-05-07 15:20:45 +0300

    add total_price and update_total_price

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -17,13 +17,15 @@ class Order(models.Model):
     address = models.CharField(
         max_length=250, help_text="введите ваш адрес", verbose_name="адрес"
     )
-    # postal_code = models.CharField(max_length=20)
     city = models.CharField(
         max_length=100, help_text="введите ваш город", verbose_name="город", blank=True
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
 
     class Meta:
         ordering = ["-created"]
@@ -36,11 +38,9 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id}:: {self.email}"
 
-    def get_total_cost(self):
-        total_cost = sum(
-            item.get_cost() for item in self.items.all()
-        )  # From OrderItem (related_name="items")
-        return total_cost
+    def update_total_price(self):
+        self.total_price = sum(item.get_cost() for item in self.items.all())
+        self.save()
 
 
 class OrderItem(models.Model):
