08bbb52680b85e49864c773458c026fae830fcff
Maks <maksimuspiter@gmail.com>
2023-05-08 11:44:23 +0300

    add delivery

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from .models import Order
+from .models import Order, Delivery
 
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["first_name", "last_name", "email", "address"]
+        fields = ["first_name", "last_name", "email", "address", "delivery"]
 
         widgets = {
             "first_name": forms.TextInput(attrs={"class": "form-control"}),
             "last_name": forms.TextInput(attrs={"class": "form-control"}),
             "email": forms.TextInput(attrs={"class": "form-control"}),
             "address": forms.TextInput(attrs={"class": "form-control"}),
-            # "city": forms.TextInput(attrs={"class": "form-control"}),
+            "delivery": forms.Select(attrs={"class": "form-control", 'readonly':'readonly'}),
         }
