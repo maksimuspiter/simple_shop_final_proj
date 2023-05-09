a11e38e94b76e835f0cdeef75d8f359feb35f3ec
Maks <maksimuspiter@gmail.com>
2023-05-09 13:12:11 +0300

    refactor  buttons

--- myshop/static/base_products/js/to_cart.js
+++ myshop/static/base_products/js/to_cart.js
@@ -18,7 +18,7 @@ function change_quantity_cart(url, action, product_id) {
   $.ajax({
     url: url,
     type: "POST",
-    data: jQuery.param({ "action": action,  "product_id":product_id}),
+    data: jQuery.param({ action: action, product_id: product_id }),
     dataType: "json",
     headers: {
       "X-Requested-With": "XMLHttpRequest",
@@ -27,9 +27,7 @@ function change_quantity_cart(url, action, product_id) {
     success: (data) => {
       console.log(data);
       if (data.result) {
-        document.getElementById(
-          "product-in-cart-quantity-" + product_id
-        ).innerHTML = data.final_quantity_in_cart;
+        change_buttons(product_id, data.final_quantity_in_cart);
         change_quantity_cart_navbar(data.all_products_in_cart_quantity);
       }
     },
@@ -45,4 +43,29 @@ function change_quantity_cart_navbar(quantity) {
     "product_in_cart_quantity_navbar"
   );
   navbar_quantity_element.innerHTML = quantity;
+}
+
+function change_buttons(product_id, quantity) {
+  let button_groap_div = document.getElementById(
+    "buttons-group-product-" + product_id
+  );
+  let button_quantity = document.getElementById(
+    "product-in-cart-quantity-" + product_id
+  );
+  switch (quantity) {
+    case 0:
+      button_groap_div.innerHTML = `<button type="button" class="btn btn-info" onclick="add_in_cart(${product_id})">В карзину</button>`;
+      break;
+    case 1:
+      button_groap_div.innerHTML = `<button class="btn btn-outline-primary btn-sm" onclick="remove_in_cart(${product_id})">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="x5-e"><path fill="currentColor" d="M5 11a1 1 0 1 0 0 2h14a1 1 0 1 0 0-2H5Z"></path></svg>
+    </button>
+    <span id="product-in-cart-quantity-${product_id}">${quantity} шт.</span>
+    <button class="btn btn-outline-primary btn-sm" onclick="add_in_cart(${product_id})">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="x5-e"><path fill="currentColor" d="M12 4a1 1 0 0 0-1 1v6H5a1 1 0 1 0 0 2h6v6a1 1 0 1 0 2 0v-6h6a1 1 0 1 0 0-2h-6V5a1 1 0 0 0-1-1Z"></path></svg>
+    </button>`;
+      break;
+    default:
+      button_quantity.innerHTML = quantity + " шт.";
+  }
 }
