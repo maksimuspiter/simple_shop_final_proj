4f1eea42c682c4d77d3c8a4995d676b0adf3be11
Maks <maksimuspiter@gmail.com>
2023-05-09 12:19:05 +0300

    change url path

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.cart_detail, name="cart_detail"),
     path("add/<int:product_id>/", views.cart_add, name="cart_add"),
     path("remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
-    path("change_ajax/<int:product_id>/", views.cart_change_ajax, name="cart-ajax"),
+    path("change_ajax/", views.cart_change_ajax, name="cart-ajax"),
 ]
