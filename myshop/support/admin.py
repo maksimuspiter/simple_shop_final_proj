ede2240a50860be4a7a2b5548293f14b6acd4cdb
Maks <maksimuspiter@gmail.com>
2023-05-09 17:36:06 +0300

    chats with messages (support replies in adminka)

--- myshop/support/admin.py
+++ myshop/support/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Status, Type, Chat, Message
+from django.contrib.admin import  widgets
+from django.db import models
+from django.forms import Textarea, TextInput 
 
-# Register your models here.
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    list_display_links = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    list_display_links = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class MessagesInline(admin.TabularInline):
+    model = Message
+    raw_id_fields = ["chat"]
+    extra = 2
+    formfield_overrides = {
+    models.TextField: {'widget': Textarea(attrs={'rows':20, 'cols':40})},
+}  
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "type",
+        "status",
+    ]
+    list_filter = ["type", "status"]
+    list_display_links = ["id", "user"]
+    inlines = [MessagesInline]
--- myshop/support/models.py
+++ myshop/support/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Статус чата"
+        verbose_name_plural = "Статусы чата"
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Тип чата"
+        verbose_name_plural = "Типы чата"
+
+    def __str__(self):
+        return self.name
+
+
+class Chat(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        related_name="chats",
+    )
+    type = models.ForeignKey(
+        Type,
+        on_delete=models.CASCADE,
+        verbose_name="Тип чата",
+    )
+    status = models.ForeignKey(
+        Status,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name="Статус чата",
+        related_name="chats",
+    )
+
+    class Meta:
+        verbose_name = "Чат"
+        verbose_name_plural = "Чаты"
+
+    def __str__(self):
+        return f"{self.user}: {self.type}"
+
+
+class Message(models.Model):
+    class Creator(models.TextChoices):
+        USER = "U", "User"
+        ADMIN = "A", "Admin"
+
+    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.CASCADE)
+    text = models.TextField(verbose_name="Текст сообщения")
+    created = models.DateTimeField(auto_now_add=True)
+
+    creator = models.CharField(
+        max_length=1,
+        choices=Creator.choices,
+        default=Creator.ADMIN,
+        verbose_name="Автор сообщения",
+    )
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+
+    def __str__(self):
+        return f"{self.chat.user}: {self.created}"
