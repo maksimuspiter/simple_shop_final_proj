0e0ddce652b07ebe486ff548547b56affacb8d36
Maks <maksimuspiter@gmail.com>
2023-05-16 14:12:04 +0300

    refactored style

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -86,16 +86,15 @@ WSGI_APPLICATION = "myshop.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'simpleshop',
-        'USER': 'simpleshop_admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "simpleshop",
+        "USER": "simpleshop_admin",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
